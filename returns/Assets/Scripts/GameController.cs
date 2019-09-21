143ea722f54109dd405c29b4dab4168cdcb3493b
Matthew Dulworth <mhdulworth@gmail.com>
2019-09-21 10:48:15 -0500

    added new gameController script that uses events instead of loops

--- returns/Assets/Scripts/GameController.cs
+++ returns/Assets/Scripts/GameController.cs
@@ -2,79 +2,159 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour 
+public class GameCo : MonoBehaviour
 {
    // --- classes --- //
-   class Package{
+   class Package
+   {
       bool legit;
       GameObject gameObject;
 
-      public bool isLegit(){
+      public bool isLegit()
+      {
          return legit;
       }
    }
-   class Customer{ 
+   class Customer
+   {
       GameObject gameObject;
       GameObject recipt;
       Package package;
       string dialouge;
 
-      public Package getPackage(){
+      public Package getPackage()
+      {
          return package;
       }
    }
-   class Manager{
+   class Manager
+   {
       string positiveDialouge;
       string gameEndDialouge;
    }
+   enum DayState
+   {
+      DAY_BEGIN,
+      RETURNS,
+      DAY_END,
+   }
+   enum Action
+   {
+      ACCEPT,
+      DENY,
+      INSPECT,
+      NONE
+   }
+
 
    // --- member vars --- //
    int funds;
-   int unhappiness;
+   int happiness;
+   DayState dayState;
+
+
    int days;
+   int currentDay;
+   int customerIndex;
+   Customer currentCustomer;
    List<Customer>[] customersForDay;
 
-   // functions
-   void changeHappiness(int value){
-       unhappiness += value;
+
+   // --- functions --- //
+   void changeHappiness(int value)
+   {
+      happiness += value;
+   }
+   void changeDayState(DayState state)
+   {
+      dayState = state;
+   }
+
+   void newCustomer(){
+
+   }
+   void accept() {
+      int value = (currentCustomer.getPackage().isLegit() ) ? +15 : +10;
+      changeHappiness(value);
+      
+      
+      newCustomer();
+   }
+   void deny() {
+
+      newCustomer();
+   }
+   void inspect() {
+
+   }
+
+   void dayBegin() {
+      customerIndex = 0;
+      currentCustomer = customersForDay[currentDay][customerIndex];
+      changeDayState(DayState.RETURNS);
+   }
+   void returns() {
+      Action selectedAction = Action.NONE;
+
+      if (Input.GetKeyDown("A")) {
+         selectedAction = Action.ACCEPT;
+      }
+      else if (Input.GetKeyDown("D")) {
+         selectedAction = Action.DENY;
+      }
+      else if (Input.GetKeyDown("I")) {
+         selectedAction = Action.INSPECT;
+      }
+
+      switch (selectedAction) {
+         case Action.ACCEPT:
+            accept();
+            break;
+         case Action.DENY:
+            deny();
+            break;
+         case Action.INSPECT:
+            inspect();
+            break;
+      }
+   }
+   void dayEnd()
+   {
+
+      changeDayState(DayState.DAY_BEGIN);
    }
    
-   // --- run once --- //
-   void Start() 
+
+   // --- Start --- //
+   void Start()
    {
-   funds = 100;
-      unhappiness = 100;
       customersForDay = new List<Customer>[days];
+      for (int i = 0; i < days; i++)
+      {
+         customersForDay[i] = new List<Customer>();
+      }
+
+      currentDay = 0;
+      funds = 100;
+      happiness = 100;
+      dayState = DayState.DAY_BEGIN;
    }
 
-   // --- run once per frame --- //
-   void Update() 
+
+   // --- Update --- //
+   void Update()
    {
-      // --- days loop --- //
-      for(int i=0; i<days; i++){
-
-         // --- returns loop --- //
-         for(int j=0; j<customersForDay[i].Count; j++){
-            Customer currentCustomer = customersForDay[i][j];
-            int changeValue = 0;
-
-            // accept
-            if(Input.GetKeyDown("A")){
-               changeValue = (currentCustomer.getPackage().isLegit())? -15 : -10;
-            }
-            // reject
-            else if(Input.GetKeyDown("D")){
-               changeValue = (currentCustomer.getPackage().isLegit())? 15 : 10;
-            }
-            // inspect option
-            else if(Input.GetKeyDown("I")){
-               
-            }
-            changeHappiness(changeValue);
-         }
-         // --- end of day --- //
-         
-
+      switch (dayState)
+      {
+         case DayState.DAY_BEGIN:
+            dayBegin();
+            break;
+         case DayState.RETURNS:
+            returns();
+            break;
+         case DayState.DAY_END:
+            dayEnd();
+            break;
       }
    }
 }
