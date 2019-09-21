7e4305d714d5c5c8471acde275fc67c33caa4743
Matthew Dulworth <mhdulworth@gmail.com>
2019-09-21 12:33:31 -0500

    added more functionality to the game controller

--- returns/Assets/Scripts/GameController.cs
+++ returns/Assets/Scripts/GameController.cs
@@ -2,74 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameCo : MonoBehaviour
+public class GameController : MonoBehaviour
 {
    // --- classes --- //
-   class Package
-   {
+   class Package {
       bool legit;
+      string descsription;
       GameObject gameObject;
 
-      public bool isLegit()
-      {
+
+      public bool isLegit() {
          return legit;
       }
+      public bool displayText(){
+
+      }
+      public bool rotate(int direction){
+         switch(direction){
+            case RIGHT:
+               break;
+            case LEFT:
+               break;
+         }
+      }
    }
-   class Customer
-   {
+   class Customer {
       GameObject gameObject;
       GameObject recipt;
       Package package;
       string dialouge;
 
-      public Package getPackage()
-      {
+      public Package getPackage() {
          return package;
       }
    }
-   class Manager
-   {
+   class Manager {
       string positiveDialouge;
       string gameEndDialouge;
    }
-   enum DayState
-   {
+   enum DayState {
       DAY_BEGIN,
       RETURNS,
       DAY_END,
    }
-   enum Action
-   {
+   enum Action {
       ACCEPT,
       DENY,
       INSPECT,
+      BACK,
       NONE
    }
 
 
    // --- member vars --- //
+   const string acceptKey = "A";
+   const string denyKey = "D";
+   const string inspectKey = "I";
+   const string backKey = "B";
+   const int LEFT = -1;
+   const int RIGHT = 1;
+
    int funds;
    int happiness;
    DayState dayState;
 
-
-   int days;
+   const int days = 3;
    int currentDay;
    int customerIndex;
    Customer currentCustomer;
    List<Customer>[] customersForDay;
 
-
    // --- functions --- //
-   void changeHappiness(int value)
-   {
+
+   // TODO: update happiness icons
+   void changeHappiness(int value) {
       happiness += value;
    }
-   void changeDayState(DayState state)
-   {
+   void changeDayState(DayState state) {
       dayState = state;
    }
-
+   
+   // TODO: make this do something
    void newCustomer(){
 
    }
@@ -77,15 +90,54 @@ public class GameCo : MonoBehaviour
       int value = (currentCustomer.getPackage().isLegit() ) ? +15 : +10;
       changeHappiness(value);
       
-      
+      customerIndex++;
       newCustomer();
    }
    void deny() {
+      int value = (currentCustomer.getPackage().isLegit() ) ? -15 : -10;
+      changeHappiness(value);
 
+      customerIndex++;
       newCustomer();
+   }
+   void back(){
+
    }
    void inspect() {
 
+      // rotate
+      int rotateDirection = 0;
+      if(Input.GetKeyDown(KeyCode.RightArrow)){
+         rotateDirection += RIGHT;
+      } 
+      if(Input.GetKeyDown(KeyCode.LeftArrow)){
+         rotateDirection += LEFT;
+      }
+      currentCustomer.getPackage().rotate(rotateDirection);
+      
+      // accept/deny/back
+      Action selectedAction = Action.NONE;
+      if (Input.GetKeyDown(acceptKey)) {
+         selectedAction = Action.ACCEPT;
+      } 
+      else if(Input.GetKeyDown(denyKey)){
+         selectedAction = Action.DENY;
+      } 
+      else if(Input.GetKeyDown(backKey)){
+         selectedAction = Action.BACK;
+      }
+
+      switch(selectedAction){
+         case Action.ACCEPT:
+            accept();
+            break;
+         case Action.DENY:
+            deny();
+            break;
+         case Action.BACK:
+            back();
+            break;
+      }
    }
 
    void dayBegin() {
@@ -96,13 +148,13 @@ public class GameCo : MonoBehaviour
    void returns() {
       Action selectedAction = Action.NONE;
 
-      if (Input.GetKeyDown("A")) {
+      if (Input.GetKeyDown(acceptKey)) {
          selectedAction = Action.ACCEPT;
       }
-      else if (Input.GetKeyDown("D")) {
+      else if (Input.GetKeyDown(denyKey)) {
          selectedAction = Action.DENY;
       }
-      else if (Input.GetKeyDown("I")) {
+      else if (Input.GetKeyDown(inspectKey)) {
          selectedAction = Action.INSPECT;
       }
 
@@ -117,20 +169,21 @@ public class GameCo : MonoBehaviour
             inspect();
             break;
       }
+
+      if(customerIndex == customersForDay[currentDay].Count){
+         changeDayState(DayState.DAY_END);
+      }
    }
-   void dayEnd()
-   {
+   void dayEnd(){
 
       changeDayState(DayState.DAY_BEGIN);
    }
    
 
    // --- Start --- //
-   void Start()
-   {
+   void Start() {
       customersForDay = new List<Customer>[days];
-      for (int i = 0; i < days; i++)
-      {
+      for (int i = 0; i < days; i++) {
          customersForDay[i] = new List<Customer>();
       }
 
@@ -142,8 +195,7 @@ public class GameCo : MonoBehaviour
 
 
    // --- Update --- //
-   void Update()
-   {
+   void Update() {
       switch (dayState)
       {
          case DayState.DAY_BEGIN:
