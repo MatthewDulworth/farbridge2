72a5e2d4ee242f6f351728770e4c688175393793
adamhleslie <adamhleslie@gmail.com>
2019-09-21 17:10:56 -0600

    Adds new scriptable object instances for characters and items, and adds new Return scriptable object type

--- returns/Assets/Scripts/Manager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Manager", menuName = "ScriptableObjects/Manager", order = 3)]
-public class Manager : ScriptableObject {
-   public string positiveDialouge;
-   public string gameEndDialouge;
-}
---
+++ returns/Assets/Scripts/ScriptableObjects/Package.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Package", menuName = "ScriptableObjects/Package", order = 2)]
+public class Package : ScriptableObject
+{
+   public GameObject gameObject;
+
+   [SerializeField]
+   private string[] descriptions;
+
+   const int UP = 1;
+   const int DOWN = -1;
+
+    public string getDescription()
+    {
+        if (descriptions.Length == 0)
+        {
+            return "";
+        }
+        else
+        {
+            return descriptions[Random.Range(0, descriptions.Length)];
+        }
+    }
+}
