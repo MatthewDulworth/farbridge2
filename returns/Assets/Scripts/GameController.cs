44d4e31ca628504097a87bfe626bd5608e1ab4e3
Matthew Dulworth <mhdulworth@gmail.com>
2019-09-20 23:09:08 -0500

    game controller

---
+++ returns/Assets/Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour 
+{
+   // classes 
+   class Package{
+      bool legit;
+      GameObject gameObject;
+   }
+   class Customer{
+      
+      GameObject gameObject;
+      GameObject package;
+      GameObject recipt;
+      string dialouge;
+   }
+   class Manager{
+
+   }
+
+
+   // member vars
+   List<GameObject> Customers;
+   int funds;
+   int unhappiness;
+   int maxCustomers;
+
+
+    // run once
+    void Start() 
+    {
+       
+    }
+
+    // run once per frame
+    void Update() 
+    {
+        
+    }
+}
