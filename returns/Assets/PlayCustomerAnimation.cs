7e18506a14c374890a227b66a0a66294310ace97
adamhleslie <adamhleslie@gmail.com>
2019-09-22 09:54:00 -0600

    Adds customer animation scrpt

---
+++ returns/Assets/PlayCustomerAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayCustomerAnimation : MonoBehaviour
+{
+    public void PlayWalkIn()
+    {
+        GetComponent<Animation>()?.Play("WalkIn");
+    }
+
+    public void PlayWalkOut()
+    {
+        GetComponent<Animation>()?.Play("Leave");
+    }
+}
