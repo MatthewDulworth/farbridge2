1e83ee0da466b4c8e8332a7ff9b1636696077f4f
adamhleslie <adamhleslie@gmail.com>
2019-09-21 21:50:35 -0600

    Adds main menu logic

---
+++ returns/Assets/MainMenuEvents.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuEvents : MonoBehaviour
+{
+    [SerializeField]
+    private string gameScene;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
